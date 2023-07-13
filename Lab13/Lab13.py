ba1af4f70d2931dee68d7384c4035b9e9921fdba
Bayasaa <bayarbayasgalan.j@gmail.com>
2023-07-13 17:41:23 -0500

    done Lab13

--- Lab13/Lab13.py
+++ Lab13/Lab13.py
@@ -1,51 +1,48 @@
 import boto3
+import json
 
-# Create the AWS clients for SNS and SQS
 sns_client = boto3.client('sns')
 sqs_client = boto3.client('sqs')
 
-# Create the SNS topic
-topic_response = sns_client.create_topic(Name='MyTopic')
+topic_response = sns_client.create_topic(Name='BaysaTopic')
 topic_arn = topic_response['TopicArn']
 
-# Create the SQS queues
-queue1_response = sqs_client.create_queue(QueueName='Queue1')
-queue2_response = sqs_client.create_queue(QueueName='Queue2')
+queue1_response = sqs_client.create_queue(QueueName='BaysaQ1')
+queue2_response = sqs_client.create_queue(QueueName='BaysaQ2')
 
-# Retrieve the queue ARNs
-queue1_arn = sqs_client.get_queue_attributes(QueueUrl=queue1_response['QueueUrl'], AttributeNames=['QueueArn'])['Attributes']['QueueArn']
-queue2_arn = sqs_client.get_queue_attributes(QueueUrl=queue2_response['QueueUrl'], AttributeNames=['QueueArn'])['Attributes']['QueueArn']
+queue1_url = queue1_response['QueueUrl']
+queue2_url = queue2_response['QueueUrl']
+queue1_arn = sqs_client.get_queue_attributes(QueueUrl=queue1_url, AttributeNames=['QueueArn'])['Attributes']['QueueArn']
+queue2_arn = sqs_client.get_queue_attributes(QueueUrl=queue2_url, AttributeNames=['QueueArn'])['Attributes']['QueueArn']
 
-# Subscribe the queues to the topic
 sns_client.subscribe(TopicArn=topic_arn, Protocol='sqs', Endpoint=queue1_arn)
 sns_client.subscribe(TopicArn=topic_arn, Protocol='sqs', Endpoint=queue2_arn)
 
-# Implement the publisher
 def publish_message(message):
+    print(f"Publishing: {message}")
     sns_client.publish(TopicArn=topic_arn, Message=message)
 
-# Implement the first consumer
 def consume_message_1():
-    response = sqs_client.receive_message(QueueUrl=queue1_response['QueueUrl'], MaxNumberOfMessages=1)
-    print (response)
-    if 'Messages' in response:
+    response = sqs_client.receive_message(QueueUrl=queue1_url, MaxNumberOfMessages=1)
+    if 'Messages' in response and len(response['Messages']) > 0:
         message = response['Messages'][0]
         body = message['Body']
+        if 'Message' in message['Body'] and type(body)==str:
+            body = json.loads(message['Body'])['Message']
         print(f"Consumer 1: {body}")
-        sqs_client.delete_message(QueueUrl=queue1_response['QueueUrl'], ReceiptHandle=message['ReceiptHandle'])
+        sqs_client.delete_message(QueueUrl=queue1_url, ReceiptHandle=message['ReceiptHandle'])
 
-# Implement the second consumer
 def consume_message_2():
-    response = sqs_client.receive_message(QueueUrl=queue2_response['QueueUrl'], MaxNumberOfMessages=1)
-    if 'Messages' in response:
+    response = sqs_client.receive_message(QueueUrl=queue2_url, MaxNumberOfMessages=1)
+    if 'Messages' in response and len(response['Messages']) > 0:
         message = response['Messages'][0]
         body = message['Body']
+        if 'Message' in message['Body'] and type(body)==str:
+            body = json.loads(message['Body'])['Message']
         print(f"Consumer 2: {body}")
-        sqs_client.delete_message(QueueUrl=queue2_response['QueueUrl'], ReceiptHandle=message['ReceiptHandle'])
+        sqs_client.delete_message(QueueUrl=queue2_url, ReceiptHandle=message['ReceiptHandle'])
 
-# Publish a message to the topic
 publish_message("hello")
 
-# Consume messages from the queues
 consume_message_1()
 consume_message_2()
