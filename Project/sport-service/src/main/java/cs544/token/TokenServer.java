805a09fffa317f3d88039ac90103ac5eee2a4c37
Bayasaa <bayarbayasgalan.j@gmail.com>
2023-07-16 21:07:43 -0500

    open port yaml

--- Project/sport-service/src/main/java/cs544/token/TokenServer.java
+++ Project/sport-service/src/main/java/cs544/token/TokenServer.java
@@ -16,59 +16,57 @@ public class TokenServer {
     
 
     public boolean verifyToken(String token) {
-        return true;
-        // ResponseEntity<String> response = connectToTokenServer(token, "verify");
-        // if (response.getStatusCode().is2xxSuccessful()) {
-        //     System.out.println("Token verification successful");
-        //     return true;
-        // } else {
-        //     System.out.println("Token verification failed");
-        //     return false;
-        // }
+        ResponseEntity<String> response = connectToTokenServer(token, "verify");
+        if (response.getStatusCode().is2xxSuccessful()) {
+            System.out.println("Token verification successful");
+            return true;
+        } else {
+            System.out.println("Token verification failed");
+            return false;
+        }
     }
 
     public String verifyTokenRole(String token) {
-        return "ROLE_ADMIN";
-        // ResponseEntity<String> response = connectToTokenServer(token, "role");
-        // if (response.getStatusCode().is2xxSuccessful()) {
-        //     // System.out.println("====="+response);
-        //     if (response.getBody().equals("User is Admin")){
-        //         // System.out.println("verifyTokenRole Token verification successful");
-        //         return "ROLE_ADMIN";
-        //     }else{
-        //         return "ROLE_USER";
-        //     }
-        // } else {
-        //     System.out.println("verifyTokenRole Token verification failed");
-        //     return "ERROR TOKEN";
-        // }
+        ResponseEntity<String> response = connectToTokenServer(token, "role");
+        if (response.getStatusCode().is2xxSuccessful()) {
+            // System.out.println("====="+response);
+            if (response.getBody().equals("User is Admin")){
+                // System.out.println("verifyTokenRole Token verification successful");
+                return "ROLE_ADMIN";
+            }else{
+                return "ROLE_USER";
+            }
+        } else {
+            System.out.println("verifyTokenRole Token verification failed");
+            return "ERROR TOKEN";
+        }
     }
 
-    // @Value("${services.auth.host}")
-    // private String authHost;
-    // @Value("${services.auth.port}")
-    // private int authPort;
-    // @Value("${services.auth.context-path}")
-    // private String authContextPath;
-    // public ResponseEntity<String> connectToTokenServer(String token, String type) {
-    //     RestTemplate restTemplate = new RestTemplate();
-    //     HttpHeaders headers = new HttpHeaders();
-    //     headers.set("Content-Type", "application/json");
-    //     String auth_url = "http://"+authHost+":"+authPort+authContextPath;
-    //     UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(auth_url)
-    //             .queryParam("token", token);
-    //     URI uri = builder.build().toUri();
-    //     HttpEntity<String> requestEntity = new HttpEntity<>(headers);
-    //     HttpMethod connection_type = HttpMethod.GET;
-    //     if (type.equals("role")){
-    //         connection_type = HttpMethod.PUT;
-    //     }
-    //     ResponseEntity<String> response = restTemplate.exchange(
-    //             uri,
-    //             connection_type,
-    //             requestEntity,
-    //             String.class);
-    //     return response;
-    // }
+    @Value("${services.auth.host}")
+    private String authHost;
+    @Value("${services.auth.port}")
+    private int authPort;
+    @Value("${services.auth.context-path}")
+    private String authContextPath;
+    public ResponseEntity<String> connectToTokenServer(String token, String type) {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", "application/json");
+        String auth_url = "http://"+authHost+":"+authPort+authContextPath;
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(auth_url)
+                .queryParam("token", token);
+        URI uri = builder.build().toUri();
+        HttpEntity<String> requestEntity = new HttpEntity<>(headers);
+        HttpMethod connection_type = HttpMethod.GET;
+        if (type.equals("role")){
+            connection_type = HttpMethod.PUT;
+        }
+        ResponseEntity<String> response = restTemplate.exchange(
+                uri,
+                connection_type,
+                requestEntity,
+                String.class);
+        return response;
+    }
 
 }
